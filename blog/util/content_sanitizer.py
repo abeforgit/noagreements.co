31b25b66779033cf6c52bd338027658978726133
abeforgit <arnebertrand@gmail.com>
2020-03-27 14:44:01 +0100

    Hook up markdown rendering and sanitizing

---
+++ blog/util/content_sanitizer.py
@@ -0,0 +1,20 @@
+import bleach
+
+_ALLOWED_TAGS = [
+    'a', 'abbr', 'acronym', 'b', 'blockquote',
+    'code', 'em', 'i', 'li', 'ol', 'strong', 'ul',
+    'iframe', 'p', 'img', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6'
+]
+_ALLOWED_ATTRS = {
+    'a': ['href', 'title'], 'abbr': ['title'],
+    'acronym': ['title'],
+    'iframe': ['style', 'src', 'seamless'],
+    'img': ['alt', 'src']
+}
+_ALLOWED_STYLES = [
+    'border', 'width', 'height'
+]
+
+
+def sanitize(content: str):
+    return bleach.clean(content, tags=_ALLOWED_TAGS, attributes=_ALLOWED_ATTRS, styles=_ALLOWED_STYLES)
--- blog/views/create_post.py
+++ blog/views/create_post.py
@@ -1,10 +1,22 @@
+import bleach
+from django.forms import Form
+from django.http import HttpResponseRedirect
 from django.views.generic import CreateView
+from markdown import markdown
 
 from blog.models import Post
+from blog.util.content_sanitizer import sanitize
 
 
 class CreatePostPage(CreateView):
+    object: Post
     model = Post
     fields = ["title", "content", "user"]
     template_name = "blog/create_post_page.html"
     success_url = "/post/{id}"
+
+    def form_valid(self, form: Form):
+        self.object = form.save(commit=False)
+        self.object.content = sanitize(markdown(self.object.content))
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
