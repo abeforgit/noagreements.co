1c8ac1ccbbc0f9043dc10702ed0563b4a1f491a4
abeforgit <arnebertrand@gmail.com>
2020-04-30 19:07:15 +0200

    Hook up posts to creator detail page

--- core/views/creator_detail.py
+++ core/views/creator_detail.py
@@ -1,7 +1,14 @@
 from django.contrib.auth.models import User
 from django.views.generic import DetailView
 
+from blog.models import Post
+
 
 class CreatorDetailPage(DetailView):
     template_name = "core/creator_detail.html"
     model = User
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["posts"] = Post.objects.filter(user_id=self.object.id)
+        return context
