869d0b8e3dcee3713ba4275cbb93e4bfe8e147d7
abeforgit <arnebertrand@gmail.com>
2020-07-10 12:39:01 +0200

    Clean up runDev script

--- runDev.sh
+++ runDev.sh
@@ -3,19 +3,19 @@
 build='true'
 
 while getopts ":n" opt; do
-    case $opt in
-        n) build=''
-           ;;
-    esac
+  case $opt in
+  n)
+    build=''
+    ;;
+  *) ;;
+  esac
 
 done
 
-shift $((OPTIND -1))
+shift $((OPTIND - 1))
 
-if [ -n $build ]
-then
-    docker-compose -f docker-compose.yml -f docker-compose-dev.yml build
+if [ -n "$build" ]; then
+  docker-compose -f docker-compose.yml -f docker-compose-dev.yml build
 fi
-
 
 docker-compose -f docker-compose.yml -f docker-compose-dev.yml up
