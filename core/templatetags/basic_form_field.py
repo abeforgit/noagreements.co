aaf973e6a043cc3720ed7aa6854780d1094588f2
abeforgit <arnebertrand@gmail.com>
2020-07-15 14:15:30 +0200

    Reorganize the create post form

---
+++ core/templatetags/basic_form_field.py
@@ -0,0 +1,10 @@
+from django import template
+from django.forms import Field
+
+register = template.Library()
+
+
+@register.inclusion_tag("core/components/basic_form_field.html")
+def basic_form_field(field: Field, id: str):
+    print(field)
+    return {"field": field, "id": id}
