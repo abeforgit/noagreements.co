191357fc2edbb9ca942dafcdddd0d3d22abd20dc
abeforgit <arnebertrand@gmail.com>
2020-10-10 19:12:16 +0200

    Add burger menu button

--- core/static/core/js/burger.js
+++ core/static/core/js/burger.js
@@ -1,13 +1,9 @@
 function openBurger() {
     let sidebar = document.getElementById("sidebar");
-    let closeWrapper = document.getElementById("close-wrapper");
-    sidebar.className = "sidebar mobile"
-    closeWrapper.className = "close-wrapper active"
+    sidebar.className = sidebar.className.replace("dn", "dib");
 }
 
 function closeBurger() {
     let sidebar = document.getElementById("sidebar");
-    let closeWrapper = document.getElementById("close-wrapper");
-    sidebar.className = "sidebar"
-    closeWrapper.className = "close-wrapper"
+    sidebar.className = sidebar.className.replace("dib", "dn");
 }
