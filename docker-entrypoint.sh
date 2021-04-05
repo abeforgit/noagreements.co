487fe24fd82c2ce93731098e90423259fd58d69a
abeforgit <arnebertrand@gmail.com>
2021-04-05 15:19:16 +0200

    Update entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env sh
 
-case $1 in
-    runprod )
-        python manage.py collectstatic --no-input
-        uvicorn noagreements.asgi:application --port 8000
-        ;;
-    rundev)
-        python manage.py runserver 0.0.0.0:8000
-        ;;
-    
-esac
+python manage.py collectstatic --no-input
+uvicorn noagreements.asgi:application --port 8000
